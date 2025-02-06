b983750405ae0e341f1a8eb102e90222d4e7d0f5
Ander Moreno <amorenoch15@gmail.com>
2025-02-06 17:39:13 +0100

    DTO javadoc done + package-info

--- src/DTO/ManagerBean.java
+++ src/DTO/ManagerBean.java
@@ -10,7 +10,9 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- *
+ * The `ManagerBean` class represents a manager in the system.
+ * It extends the `UserBean` class and implements the `Serializable` and `Cloneable` interfaces.
+ * 
  * @author Ander
  * @author Aitziber
  */
@@ -20,59 +22,112 @@ public class ManagerBean extends UserBean implements Serializable, Cloneable {
     private static final long serialVersionUID = 1L;
     private boolean isActive;
     private String password;
-    //private List<PurchaseEntity> purchases;
     private List<AnimalGroupBean> animalGroups;
 
+    /**
+     * Default constructor for the `ManagerBean` class.
+     */
     public ManagerBean() {
     }
 
+    /**
+     * Parameterized constructor for the `ManagerBean` class.
+     * 
+     * @param isActive Indicates whether the manager is active.
+     * @param password The manager's password.
+     * @param name The manager's name.
+     * @param email The manager's email.
+     * @param phone The manager's phone number.
+     * @param city The manager's city.
+     * @param zip The manager's zip code.
+     * @param street The manager's street address.
+     */
     public ManagerBean(boolean isActive, String password, String name, String email, String phone, String city, String zip, String street) {
         super(name, email, phone, city, zip, street);
         this.isActive = isActive;
         this.password = password;
     }
-     public ManagerBean clone() throws CloneNotSupportedException{
+
+    /**
+     * Creates and returns a copy of this `ManagerBean` object.
+     * 
+     * @return A clone of this `ManagerBean` instance.
+     * @throws CloneNotSupportedException If cloning is not supported.
+     */
+    public ManagerBean clone() throws CloneNotSupportedException {
         return (ManagerBean) super.clone();
     }
 
+    /**
+     * Returns the ID of the manager.
+     * 
+     * @return The manager's ID.
+     */
     @Override
     public Long getId() {
         return super.getId();
     }
 
+    /**
+     * Sets the ID of the manager.
+     * 
+     * @param id The manager's ID.
+     */
     @Override
     public void setId(Long id) {
         super.setId(id);
     }
 
+    /**
+     * Checks if the manager is active.
+     * 
+     * @return `true` if the manager is active, `false` otherwise.
+     */
     public boolean isIsActive() {
         return isActive;
     }
 
+    /**
+     * Sets the active status of the manager.
+     * 
+     * @param isActive The active status to set.
+     */
     public void setIsActive(boolean isActive) {
         this.isActive = isActive;
     }
 
+    /**
+     * Returns the manager's password.
+     * 
+     * @return The manager's password.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the manager's password.
+     * 
+     * @param password The password to set.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-//
-//    public List<PurchaseEntity> getPurchases() {
-//        return purchases;
-//    }
-//
-//    public void setPurchases(List<PurchaseEntity> purchases) {
-//        this.purchases = purchases;
-//    }
 
+    /**
+     * Returns the list of animal groups associated with the manager.
+     * 
+     * @return The list of `AnimalGroupBean` objects.
+     */
     public List<AnimalGroupBean> getAnimalGroups() {
         return animalGroups;
     }
 
+    /**
+     * Sets the list of animal groups associated with the manager.
+     * 
+     * @param animalGroups The list of `AnimalGroupBean` objects to set.
+     */
     public void setAnimalGroups(List<AnimalGroupBean> animalGroups) {
         this.animalGroups = animalGroups;
     }
@@ -86,7 +141,6 @@ public class ManagerBean extends UserBean implements Serializable, Cloneable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof ManagerBean)) {
             return false;
         }
@@ -97,9 +151,13 @@ public class ManagerBean extends UserBean implements Serializable, Cloneable {
         return true;
     }
 
+    /**
+     * Returns a string representation of the `ManagerBean` object.
+     * 
+     * @return A string containing the manager's details.
+     */
     @Override
     public String toString() {
         return "Manager[ id=" + id + " name=" + name + " email=" + email + " password=" + password + " address=" + street + " city=" + city + " zip=" + zip + "]";
     }
-
 }
