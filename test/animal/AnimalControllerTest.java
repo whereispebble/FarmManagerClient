ef1507af9ea756e980ab34892d4fc5405d3a7139
aitziberE <eskizabel.aitziber@gmail.com>
2025-02-07 06:13:36 +0100

    add animal tests

--- test/animal/AnimalControllerTest.java
+++ test/animal/AnimalControllerTest.java
@@ -6,21 +6,14 @@
 package animal;
 
 import DTO.AnimalBean;
-import java.util.List;
-import java.util.Random;
 import java.util.logging.Logger;
-import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -30,17 +23,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.runner.RunWith;
 import static org.testfx.api.FxAssert.verifyThat;
 import org.testfx.framework.junit.ApplicationTest;
-import static org.testfx.matcher.base.NodeMatchers.isDisabled;
-import static org.testfx.matcher.base.NodeMatchers.isEnabled;
 import static org.testfx.matcher.base.NodeMatchers.isVisible;
-import static org.testfx.matcher.control.ButtonMatchers.isCancelButton;
-import static org.testfx.matcher.control.ButtonMatchers.isDefaultButton;
-import static org.testfx.matcher.control.ComboBoxMatchers.hasSelectedItem;
 import static org.testfx.matcher.control.TextInputControlMatchers.hasText;
 
 
@@ -53,30 +38,28 @@ import static org.testfx.matcher.control.TextInputControlMatchers.hasText;
 public class AnimalControllerTest extends ApplicationTest{
     
     private static final Logger logger = Logger.getLogger(AnimalControllerTest.class.getName());
+    private static boolean loggedIn = false;
     
-//    AnimalBean dummy = new AnimalBean();
-
     @Override
     public void start(Stage stage) throws Exception {
-        logger.info("iniciando desde animaltest");
-        
-        Parent root = FXMLLoader.load(getClass().getResource("/ui/view/SignIn.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();    
+        if (!loggedIn) {
+            Parent root = FXMLLoader.load(getClass().getResource("/ui/view/SignIn.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
     @Test
     public void testA_OpenAnimalView() {
-        logger.info("Probando ingreso de credenciales y cambio de ventana...");
+        clickOn("#tfUsername").write("aitziber@paia.com");
+        verifyThat("#tfUsername", hasText("aitziber@paia.com"));
 
-        clickOn("#tfUsername").write("manager1@example.com");
-        verifyThat("#tfUsername", hasText("manager1@example.com"));
-
-        clickOn("#pfPasswd").write("manager123");
-        verifyThat("#pfPasswd", hasText("manager123"));
+        clickOn("#pfPasswd").write("12345678");
+        verifyThat("#pfPasswd", hasText("12345678"));
 
         clickOn("#btnSignIn");
+        loggedIn = true;
 
         verifyThat("#menuBar", isVisible());
         clickOn("#menuNavigateTo");
@@ -101,28 +84,84 @@ public class AnimalControllerTest extends ApplicationTest{
     }
     
     @Test
-    public void testC_DeleteAniaml() {
-        logger.info("Probando eliminación de animal...");
+    public void testC_AddAnimal() {
+        TableView<AnimalBean> table = lookup("#tbAnimal").query();
+        int initialSize = table.getItems().size();
+        clickOn("#btnAdd");
+        
+        write("NEW"); 
+        push(KeyCode.ENTER);
 
+        ObservableList<AnimalBean> items = table.getItems();
+        boolean newAnimalAdded = items.stream().anyMatch(item -> "NEW".equals(item.getName()));
+        assertTrue("El animal con el nombre 'NEW' debería estar presente en la tabla", newAnimalAdded);
+
+        assertEquals("La tabla debería tener una fila más", initialSize + 1, table.getItems().size());
+
+    }
+ 
+    @Test
+    public void testB_UpdateAnimal() {
         TableView<AnimalBean> table = lookup("#tbAnimal").query();
 
-    //    AnimalBean itemToDelete = table.getItems().get(0);
-//
-//        Node row=lookup(".table-row-cell").nth(0).query();
-//        clickOn(row);
+        ObservableList<AnimalBean> items = table.getItems();
+        AnimalBean animalToUpdate = items.stream()
+                                         .filter(item -> "NEW".equals(item.getName()))
+                                         .findFirst()
+                                         .orElse(null);
 
+        assertNotNull("No se encontró el animal con el nombre 'NEW'", animalToUpdate);
+
+        TableRow<AnimalBean> row = lookup(".table-row-cell").nth(items.indexOf(animalToUpdate)).query();
+        Node cell = from(row).lookup(".table-cell").nth(0).query();
+        clickOn(cell); 
+         clickOn(cell);
+        write("TEST");
+        push(KeyCode.ENTER);
+
+        items = table.getItems();
+        boolean animalUpdated = items.stream().anyMatch(item -> "TEST".equals(item.getName()));
+        assertTrue("El animal con el nombre 'TEST' debería estar presente en la tabla", animalUpdated);
+    }
+
+    @Test
+    public void testE_DeleteAniaml() {
+        TableView<AnimalBean> table = lookup("#tbAnimal").query();
         TableRow<AnimalBean> row = lookup(".table-row-cell").nth(0).query();
         clickOn(row);
 
-        AnimalBean selectedAnimal=(AnimalBean)table.getSelectionModel()
-                                         .getSelectedItem();
+        AnimalBean selectedAnimal=(AnimalBean)table.getSelectionModel().getSelectedItem();
         rightClickOn(row);
-//        clickOn("#miDelete");
-//
-//        ObservableList<AnimalBean> items = table.getItems();
-//        boolean isDeleted = items.stream().noneMatch(item -> item.equals(selectedAnimal));
-//
-//        assertTrue("El item eliminado debería ya no estar presente en la tabla", isDeleted);
+        clickOn("#miDelete");
+        clickOn("Sí");
+        
+        ObservableList<AnimalBean> items = table.getItems();
+        boolean isDeleted = items.stream().noneMatch(item -> item.equals(selectedAnimal));
+
+        assertTrue("El item eliminado no debería estar presente en la tabla", isDeleted);
     }
     
+    @Test
+    public void testC_DeleteAnimal() {
+        TableView<AnimalBean> table = lookup("#tbAnimal").query();
+
+        ObservableList<AnimalBean> items = table.getItems();
+        AnimalBean animalToDelete = items.stream()
+                                        .filter(item -> "TEST".equals(item.getName()))
+                                        .findFirst()
+                                        .orElse(null);
+
+        assertNotNull("No se encontró el animal con el nombre 'TEST'", animalToDelete);
+
+        TableRow<AnimalBean> row = lookup(".table-row-cell").nth(items.indexOf(animalToDelete)).query();
+        clickOn(row);
+
+        rightClickOn(row);
+        clickOn("#miDelete");
+        clickOn("Sí");
+
+        items = table.getItems();
+        boolean animalDeleted = items.stream().noneMatch(item -> "TEST".equals(item.getName()));
+        assertTrue("El animal con el nombre 'TEST' debería haber sido eliminado de la tabla", animalDeleted);
+    }
 }
